d53e28e664e359cd5441691fe3d2f8483e7454e6
Farihul Rouf <farihulrouf@gmail.com>
2024-01-10 21:10:53 +0700

    init

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const con = require('./config/condb');
-//const mongoString = process.env.DATABASE_URL;
-
-//mongoose.connect(mongoString);
-/*
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error)
-})
-
-database.once('connected', () => {
-    console.log('Database Connected');
-})
-*/
-
 const app = express();
 app.use(cors())
 app.use(express.json());
@@ -25,14 +10,16 @@ app.use(express.json());
 const customers = require('./routes/customers')
 const transactions = require('./routes/transactions')
 const users = require('./routes/users')
-
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+})
 app.use('/customers', customers)
 app.use('/transactions', transactions)
 app.use('/users', users)
 
 
 //app.use('/users', users)
-app.listen(PORT, () => {
+app.listen(3000, () => {
     //console.log(`Server Started at ${3000}`)
 })
 
